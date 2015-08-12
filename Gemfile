62d9a90e1bc87ead74cee483e37f829ef42b8f4f
zekokh <heroin160892>
2015-08-13 02:27:00 +0300

    fixed get access_token

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
-
+gem 'pry-rails'
 gem 'rest-client'
 gem 'json'
 gem 'instagram'
--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -6,27 +6,25 @@ class TokenController < ApplicationController
     code = info.code
     puts(client_id, client_secret, code)
     begin
-    #response = RestClient.get 'https://api.instagram.com/oauth/authorize/?client_id=24626bd21aa74c0f8af7d7e41b992323&redirect_uri=http://localhost:3000/information_to_connects&response_type=code'
-    response = RestClient.post 'https://api.instagram.com/oauth/access_token', client_id: client_id, client_secret: client_secret, grant_type: 'authorization_code', redirect_uri: 'http://localhost:3000/information_to_connects', code: code
-    puts("Тут ответ")
-    puts(response)
-    json = JSON.parse(response)
-    puts(json)
-    access_token = json['access_token']
-    puts(access_token)
-    if access_token
-      info.access_token=access_token
-      access_token.save
-      redirect_to information_to_connects_path
-    else
-      puts("тут ошибка")
+      #response = RestClient.get 'https://api.instagram.com/oauth/authorize/?client_id=24626bd21aa74c0f8af7d7e41b992323&redirect_uri=http://localhost:3000/information_to_connects&response_type=code'
+      params_to_access_token = { client_id: client_id,
+                 client_secret: client_secret,
+                 grant_type: 'authorization_code',
+                 redirect_uri: 'http://localhost:3000/information_to_connects',
+                 code: code }
+      json ||= JSON.parse(RestClient.post 'https://api.instagram.com/oauth/access_token', params_to_access_token)
+      access_token = json['access_token']
       puts(access_token)
-      redirect_to information_to_connects_path, notice: 'Произошла неведомая ошибка!'
-      puts("access_token нет, произошла ошибка")
-      puts(json)
-    end
+      if info.update_attribute(:access_token, access_token)
+        redirect_to information_to_connects_path
+      else
+        redirect_to information_to_connects_path, notice: 'Произошла неведомая ошибка!'
+      end
     rescue => e
       redirect_to information_to_connects_path, notice: "Ошибка: #{e.response}"
     end
   end
 end
+
+
+
