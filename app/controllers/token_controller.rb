dffda5108951cce430ad276ebc8e476cb23aebd9
zekokh <heroin160892>
2015-08-13 01:41:40 +0300

    add some

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,32 +1,20 @@
 class TokenController < ApplicationController
   def get_token
-    info = InformationToConnect.first
-    client_id = info.client_id
-    client_secret = info.client_secret
-    code = info.code
-    puts(client_id, client_secret, code)
-    begin
-    #response = RestClient.get 'https://api.instagram.com/oauth/authorize/?client_id=24626bd21aa74c0f8af7d7e41b992323&redirect_uri=http://localhost:3000/information_to_connects&response_type=code'
-    response = RestClient.post 'https://api.instagram.com/oauth/access_token', client_id: client_id, client_secret: client_secret, grant_type: 'authorization_code', redirect_uri: 'http://localhost:3000/information_to_connects', code: code
-    puts("Тут ответ")
-    puts(response)
+=begin
+    http = Net::HTTP.new('https://api.instagram.com')
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new('/oauth/access_token')
+    request.body = {:client_id => '24626bd21aa74c0f8af7d7e41b992323', :client_secret => 'dec1fe820eff428c94d2fd4803551f04', :grant_type => 'authorization_code', :code => '944fdb3ee98c4403887f758a3877c12f'}
+    response = http.request(request)
     json = JSON.parse(response)
+    puts("Тут ответ")
     puts(json)
-    access_token = json['access_token']
-    puts(access_token)
-    if access_token
-      info.access_token=access_token
-      access_token.save
-      redirect_to information_to_connects_path
-    else
-      puts("тут ошибка")
-      puts(access_token)
-      redirect_to information_to_connects_path, notice: 'Произошла неведомая ошибка!'
-      puts("access_token нет, произошла ошибка")
-      puts(json)
-    end
-    rescue => e
-      redirect_to information_to_connects_path, notice: "Ошибка: #{e.response}"
-    end
+=end
+
+    response = RestClient.post("https://api.instagram.com/oauth/access_token", nested: {client_id: "24626bd21aa74c0f8af7d7e41b992323", client_secret: "dec1fe820eff428c94d2fd4803551f04",
+                               grant_type: "authorization_code", code: "12e7033d3f6045058c2805cab8dff4ae"})
+    puts("тут смотри")
+    puts(response.code)
   end
 end
