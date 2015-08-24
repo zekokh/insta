a192849c58e422c006d2bc0c6416527db7d3616c
zekokh <heroin160892>
2015-08-24 11:40:01 +0300

    add flag

--- app/services/manager.rb
+++ app/services/manager.rb
@@ -10,13 +10,15 @@ class Manager
       if @stock.is_valid
         Members.search(@tag, @info.access_token)
       else
-
+        @flag = true # флаг что бы запускать метод поиска участников один раз (что бы сохранялось количество лайков)
+        if @flag #todo велосипед переписать
+          Members.search(@tag, @info.access_token)
+          @flag = false
+        end
       end
     end
   end
-
   def Manager.stop
     Members.search(@tag, @info.access_token)
   end
-
 end
