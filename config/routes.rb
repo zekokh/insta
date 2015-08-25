963cb647bab6693b36180048698f3076f5321f31
zekokh <heroin160892>
2015-08-25 11:28:47 +0300

    add members code in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Insta::Application.routes.draw do
   root 'information_to_connects#index'
 
+  get '/member/:id', to: 'members#show'
+  get '/change/:id', to: 'member#change'
   get '/get_token', to: 'token#get_token'
   get '/get_tags', to: 'tags#get_tags'
   get '/members', to: 'members#get_members'
   get '/members_true', to: 'members#member_true'
   get '/members_false', to: 'members#member_false'
-  get '/member/:id', to: 'members#show'
-  get '/change/:id', to: 'member#change'
+
 
   resources :home, :information_to_connects, :stocks
   resources :sessions, only: [:new, :create, :destroy]
